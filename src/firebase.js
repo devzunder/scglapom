c2f3724942da5598c96b73cc70c96c6ef2981656
Lucas Zunder <35578650+devzunder@users.noreply.github.com>
2018-10-01 22:07:14 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import 'firebase/database';
 
   // Initialize Firebase
   const config = {
-    apiKey: "AIzaSyCpNA2ozr0gYN8OKeuKOfYYUWgQ4_fY_vA",
-    authDomain: "tccscglapom.firebaseapp.com",
-    databaseURL: "https://tccscglapom.firebaseio.com",
-    projectId: "tccscglapom",
-    storageBucket: "tccscglapom.appspot.com",
-    messagingSenderId: "482955133339"
+    apiKey: "---------------------",
+    authDomain: "-------------------------",
+    databaseURL: "----------------------------",
+    projectId: "---------------------",
+    storageBucket: "-----------------------",
+    messagingSenderId: "----------------------"
   };
   firebase.initializeApp(config);
 
